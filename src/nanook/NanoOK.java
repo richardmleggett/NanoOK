b08be5d7ddc38e862280176512b6024d02b50cbe
leggettr <richard.leggett@tgac.ac.uk>
2016-02-06 21:12:54 +0000

    MAXK update

--- src/nanook/NanoOK.java
+++ src/nanook/NanoOK.java
@@ -25,7 +25,7 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @author Richard Leggett
  */
 public class NanoOK {
-    public final static String VERSION_STRING = "v0.62";
+    public final static String VERSION_STRING = "v0.63";
     public final static long SERIAL_VERSION = 3L;
     
     /**
